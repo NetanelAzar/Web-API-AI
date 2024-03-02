5744e61c6762799946b0aef290fad1ccd61a13a3
AZAR <49813490+NetanelAzar@users.noreply.github.com>
2024-03-02 23:50:56 +0200

    first commit

--- API/V1/controllers/text_mongo.js
+++ API/V1/controllers/text_mongo.js
@@ -1,30 +1,39 @@
-const text = require("../models/text");
+const text = require("../models/text"); // יבוא מודל text
+
 module.exports = {
   getAllTexts: (req, res) => {
+    // פונקציה לקבלת כל הטקסטים
     text.find().then((data) => {
-      return res.status(200).json(data);
+      // מציאת כל המסמכים במודל text
+      return res.status(200).json(data); // החזרת נתוני המסמך בפורמט JSON
     });
   },
 
   getTextById: (req, res) => {
-    let Tid = req.params.id;
+    // פונקציה לקבלת מסמך לפי מזהה
+    let Tid = req.params.id; // השגת מזהה מהבקשה
     text.findOne({ Tid }).then((data) => {
-      return res.status(200).json(data);
+      // מציאת מסמך לפי המזהה
+      return res.status(200).json(data); // החזרת המסמך בפורמט JSON
     });
   },
 
   updateText: (req, res) => {
-    let Tid = req.params.id;
-    let body = req.body;
+    // פונקציה לעדכון מסמך
+    let Tid = req.params.id; // השגת מזהה מהבקשה
+    let body = req.body; // שינויים המתקבלים מהבקשה
     text.updateOne({ Tid }, body).then((data) => {
-      return res.status(200).json(data);
+      // עדכון המסמך לפי המזהה
+      return res.status(200).json(data); // החזרת נתונים על העדכון בפורמט JSON
     });
   },
 
   deleteText: (req, res) => {
-    let Tid = req.params.id;
+    // פונקציה למחיקת מסמך
+    let Tid = req.params.id; // השגת מזהה מהבקשה
     text.deleteOne({ Tid }).then((data) => {
-      return res.status(200).json(data);
+      // מחיקת המסמך לפי המזהה
+      return res.status(200).json(data); // החזרת נתונים על המחיקה בפורמט JSON
     });
   },
 };
