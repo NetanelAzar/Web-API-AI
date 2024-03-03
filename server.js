941bde0c7354fc7a0c17005ae8399de41c383cea
AZAR <49813490+NetanelAzar@users.noreply.github.com>
2024-03-03 02:41:19 +0200

    comment

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+// יבוא של המודול http שברשות Node.js
 const http = require("http");
+
+// יבוא של המודול app מהקובץ המצוי באותו תיקייה
 const app = require("./app");
+
+// השמת הפורט המוגדר בסביבת העבודה למשתנה port
 const port = process.env.PORT;
+
+// יצירת השרת על בסיס האפליקציה שנוצרה
 const server = http.createServer(app);
 
+// האזנה לחיבורים נכנסים והדפסת הודעה בעת עליית השרת
 server.listen(port, () => {
   console.log("Server is running ");
 });
