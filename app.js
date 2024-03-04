58f87663929b8716d10b0bfd3038d2670c8c54ac
AZAR <49813490+NetanelAzar@users.noreply.github.com>
2024-03-04 08:02:27 +0200

    remove home

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.get("/user", (req, res) => {
     });
 });
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   return res.status(200).render("home", { layout: "main", title: "Home" });
 });
 
